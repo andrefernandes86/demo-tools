69d5d918b825891a7df8a451712f463ade68f402
Andre Fernandes <andre.fernandes86@gmail.com>
2022-05-14 11:10:44 -0500

    Update demo-tools.sh

--- demo-tools.sh
+++ demo-tools.sh
@@ -84,7 +84,7 @@ case $choice in
     echo '---------- Web App Tools ----------'
     echo ' ' 
     echo 'Running the Web App Tools..'  
-    docker run --rm -d -p 222:22 -p 7331:7331 -p 9292:9292 arachni/arachni:latest --name webtools
+    docker run --rm -d  --name webtools -p 222:22 -p 7331:7331 -p 9292:9292 arachni/arachni:latest
     echo ' ' 
     echo ' URL: http://$ip-address:9292 '
     echo ' Username: admin@admin.admin '
